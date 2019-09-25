a7f435c9fbaadb0b6d287a94e6cbec0173a55874
nutchapon <nutchapon.han@gmail.com>
2019-09-25 10:55:40 +0700

    finish sync front and back

--- smartbrainapp/src/component/FaceRecognition/FaceRecognition.js
+++ smartbrainapp/src/component/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({box, imageUrl}) =>{
     return(
         <div className='center'>
             <div className='absolute mt2'>
-                <img id='inputimage' src={imageUrl} alt='pic' width='500px' height='auto' />
+                <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
                 <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
